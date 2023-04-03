d769107f6250a6b54b882df2d4a28e1e01a0ca08
milenaszdl <milicca070803@mail.ru>
2023-04-04 02:59:35 +0300

    лаба по ноду

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 const http = require('node:http');
 
-const host = "127.0.0.1"
-const port = 5500
+const host = "127.0.0.1";
+const port = 5500;
 
-const users = [{id:1, name: "Vasya"}, {id:2, name: "Milena"}]
+var stats = {user_agent:0};
 
 const server = http.createServer((req,res)=>{
     if (req.url === "/") {
         res.statusCode = 201;
         res.setHeader("Content-type", "text/plain");
-        res.end("Hi");
-    }else if (req.url === "/user") {
-        if (req.method === "GET") {
+        res.end("Hi!");
+    }else if (req.method === "GET") {
+        if (req.url === "/stats") {
             res.statusCode = 200;
-            res.setHeader("Content-type", "application/json");
-            res.end(JSON.stringify(users));
-        } else if (req.method === "POST") {
+            stats.user_agent++;
+            res.setHeader("Content-Type", "text/html");
+            res.end(`<table>
+            <tr>
+                <th>User-agent:</th>
+                <th>Number of requests:</th>
+            </tr>
+            <tr>
+                <td>${req.headers['user-agent']}</td>
+                <td>${stats.user_agent}</td>
+            </tr>
+            </table>`);
+        }
+    }else if (req.url === "/comments") {
+        if (req.method === "POST") {
             body = "";
             req.on("data", (chunk)=>{
                 body+= chunk.toString();
             })
             req.on("end", ()=>{
-                res.end(body);
+                res.end(body); //полученные сервером данные выводим обратно клиенту
+                console.log(body); //выводим данные, полученные на сервер в консоль
             })
-        } else{
+        }
+    } else{
             res.statusCode = 400;
             res.setHeader("Content-type", "text/plain");
-            res.end("Bad method");
+            res.end("Bad request");
         }
-    }else {
-        res.statusCode = 404;
-        res.setHeader("Content-type", "text/plain");
-        res.end("Not found");
-    }
-
-    // res.statusCode = 200;
-    // res.setHeader("Content-type", "text/html");
-    // res.end("{status: 400, message: error}");
-});
+    })
 
 server.on("connection", ()=> {
     console.log("Новое подключение");
