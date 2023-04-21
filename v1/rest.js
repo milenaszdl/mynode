efbc9f448f6b597804f090d712171c9cbc6f88dd
milenaszdl <milicca070803@mail.ru>
2023-04-21 18:06:09 +0300

    middleware

--- v1/rest.js
+++ v1/rest.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const jsonParser = bodyParser.json({
+    extended: false,
+});
 
 var stats = {user_agent:0};
 var comments = [];
+
+const user = {name: "milena", email: "milicca0708083@mail.ru", password: "milena228"};
 
 router.get('/', function(req,res) {
     res.send('Привет');
@@ -24,10 +33,41 @@ router.get('/stats', function(req,res) {
     </table>`);
 })
 
+function CheckLogin(req,res,next){
+    if (req.body.name == user.name){
+        next();
+    } else {
+        res.send("Invalid login");
+    }
+}
+
+router.post('/login', jsonParser, CheckLogin, (req,res) => {
+    res.status(228).send(`Welcome, ${user.name}`);
+})
+
 router.post('/comments', express.json(),function(req,res) {
     comments.push(req.body.name, req.body.comment);
     res.status(201);
     res.send(JSON.stringify(comments));
 })
+
+function CheckAuthorisation(req, res, next){
+    const apiKey = req.query.apiKey;
+    if (apiKey === 'getauthorised'){
+        next();
+    } else {
+        res.send("Authorisation denied");
+    }
+}
+
+router.post('/author', CheckAuthorisation, (req,res) => {
+    res.status(215).send("Authorisation passed");
+})
+
+
+router.use((req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.status(404).send("Not found");
+});
 
 module.exports = router;
